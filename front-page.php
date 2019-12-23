db57aba8162444672bb06cf10fcbc5f451ea1ad9
nilesh-bitcot <nilesh.bitcot@gmail.com>
2019-12-23 23:55:18 +0530

    1st commit in staging

--- front-page.php
+++ front-page.php
@@ -16,15 +16,15 @@ get_header(); ?>
 					echo '<p>No content found</p>';
 
 				endif; ?>
-				
+
 				<!-- home-columns -->
 				<div class="home-columns clearfix">
-					
+
 					<!-- one-half -->
 					<div class="one-half">
-						
+
 						<h2>Latest Opinion</h2>
-						
+
 						<?php // opinion posts loop begins here
 						$ourPosts = new WP_Query('cat=3&posts_per_page=2');
 
@@ -57,16 +57,16 @@ get_header(); ?>
 								// fallback no content message here
 						endif;
 						wp_reset_postdata(); ?>
-						
+
 						<span class="horiz-center"><a href="<?php echo get_category_link(3); ?>" class="btn-a">View all Opinion Posts</a></span>
-						
+
 					</div><!-- /one-half -->
-					
+
 					<!-- one-half -->
 					<div class="one-half last">
-						
+
 						<h2>Latest News</h2>
-						
+
 						<?php // news posts loop begins here
 						$ourPosts = new WP_Query('cat=4&posts_per_page=2');
 
@@ -101,15 +101,15 @@ get_header(); ?>
 						wp_reset_postdata();
 
 						?>
-						
+
 						<span class="horiz-center"><a href="<?php echo get_category_link(4); ?>" class="btn-a">View all News Posts</a></span>
-						
+
 					</div><!-- /one-half -->
-					
+
 				</div><!-- /home-columns -->
-			
+
 	</div><!-- /site-content -->
-	
+
 	<?php get_footer();
 
 ?>
